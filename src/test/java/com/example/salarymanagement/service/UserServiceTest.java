88e89fb1def3a6613ef7e262a708bbe04c0ecc6e
Javen Seow <javenseow@gmail.com>
2021-04-24 23:44:33 +0800

    Add unit tests for getUser method

--- src/test/java/com/example/salarymanagement/service/UserServiceTest.java
+++ src/test/java/com/example/salarymanagement/service/UserServiceTest.java
@@ -12,6 +12,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,6 +25,27 @@ class UserServiceTest {
 
     @InjectMocks
     private UserService userService;
+
+    @Test
+    void getUser_shouldReturnUserWithExistingID() {
+       Mockito.when(userRepository.findById(Utility.validId)).thenReturn(Optional.of(Utility.validUser));
+       User result = userService.getUser(Utility.validId);
+
+       assertEquals(Utility.validUser, result);
+    }
+
+    @Test
+    void getUser_shouldThrowExceptionWithNonExistingID() {
+        Mockito.when(userRepository.findById(Utility.validId)).thenReturn(Optional.empty());
+        Exception e = assertThrows(IllegalStateException.class, () -> {
+            userService.getUser(Utility.invalidId);
+        });
+
+        String expectedMessage = Response.NO_SUCH_EMPLOYEE;
+        String actualMessage = e.getMessage();
+
+        assertTrue(expectedMessage.contains(actualMessage));
+    }
 
     @Test
     void deleteUser_shouldDeleteSuccessfullyWithExistingID() {
@@ -44,5 +67,17 @@ class UserServiceTest {
         String actualMessage = e.getMessage();
 
         assertTrue(expectedMessage.contains(actualMessage));
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void checkDuplicates() {
+    }
+
+    @Test
+    void checkDataChanged(){
     }
 }
